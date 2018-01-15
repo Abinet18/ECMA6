4cee5f373d0e43ac5e20e1fe5e0b1ba63647f6c6
unknown <abinet.tafa@gmail.com>
2018-01-15 17:52:25 -0600

    added curry.js

---
+++ curry.js
@@ -0,0 +1,12 @@
+var applyCoupon = (category) => (discount)=>(item)=>{
+    item.price=item.price*(1-discount);
+    return item;
+}
+
+const item= {
+    "name": "Biscuits",
+    "type" : "regular",
+    "category" : "food",
+    "price" : 2.0
+}
+console.log(applyCoupon("food")(0.1)(item).price===1.8);
